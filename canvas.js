1a17beebcd27d7f985d05e1c90196536e73eb9dc
otaviooalmeida <161171224+otaviooalmeida@users.noreply.github.com>
2024-09-25 13:53:39 -0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -6,40 +6,6 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-// c.fillRect(100, 100, 100, 100);
-// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-// c.fillRect(400, 100, 100, 100);
-// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-// c.fillRect(300, 300, 100, 100);
-
-// // Line
-
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = 'aqua';
-// c.stroke();
-
-// // Arc and circle
-
-// c.beginPath();
-// c.arc(300, 300, 290, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-
-
-
-// for (var i = 0; i < 3; i++) {
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight; 
-//     c.beginPath();
-//     c.arc(x, y, 50, 0, Math.PI * 2, false);
-//     c.strokeStyle = 'red';
-//     c.stroke();
-// };
-
 
 function Circle(x, y, dx, dy, radius) {
     this.x = x;
